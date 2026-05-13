634514df29c707311221a7546c9375b997c0c5d8
df275 <df275@EVIL>
2026-05-13 23:50:25 +0500

    Класс афишы и коннект к БД. Чрнышков

---
+++ KINO_Chernyshkov/Classes/Common/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace KINO_Chernyshkov.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=127.0.0.1;uid=root;pwd=;datebase=kino";
+
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+
+            connection.Open();
+
+            return connection;
+
+        }
+
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
